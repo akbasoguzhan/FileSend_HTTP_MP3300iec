62906538d8a41f9d56a7fbb9726a337d1e706f53
akbasoguzhan <oguzhanakbas8198@gmail.com>
2023-07-18 20:09:20 +0300

    Add project files.

---
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,151 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class FileSender
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.ControllerFolder = new System.Windows.Forms.ComboBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(25, 50);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(460, 26);
+            this.textBox1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(21, 25);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(294, 20);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Gönderilecek Dosya Yolunu Seçiniz";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(360, 201);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(125, 58);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Send To Controller";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(26, 144);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(138, 20);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "IP Adresi Giriniz";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox2.Location = new System.Drawing.Point(25, 169);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(460, 26);
+            this.textBox2.TabIndex = 3;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(436, 50);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(49, 26);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "Seç";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // ControllerFolder
+            // 
+            this.ControllerFolder.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ControllerFolder.FormattingEnabled = true;
+            this.ControllerFolder.Location = new System.Drawing.Point(25, 106);
+            this.ControllerFolder.Margin = new System.Windows.Forms.Padding(2);
+            this.ControllerFolder.Name = "ControllerFolder";
+            this.ControllerFolder.Size = new System.Drawing.Size(460, 28);
+            this.ControllerFolder.TabIndex = 15;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(21, 84);
+            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(143, 20);
+            this.label3.TabIndex = 14;
+            this.label3.Text = "Controller Folder";
+            // 
+            // FileSender
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(497, 271);
+            this.Controls.Add(this.ControllerFolder);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Name = "FileSender";
+            this.Text = "FileSender";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.ComboBox ControllerFolder;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Net;
+using System.Windows.Forms;
+using System.Net.Http;
+using Yaskawa.IEC61131.RMI.HttpUtil;
+
+namespace WindowsFormsApp1
+{
+    public partial class FileSender : Form
+    {
+        private IUserFileUtility utility_;
+        public FileSender()
+        {
+            InitializeComponent();
+            ControllerFolder.Items.Clear();
+            ControllerFolder.Items.AddRange(new string[] { UserDirectories.FLASH_DRIVE_PARAMETERS, UserDirectories.FLASH_USER_DATA, UserDirectories.FLASH_CAM_DATA, UserDirectories.RAMDISK_USER_DATA, UserDirectories.RAMDISK_CAM_DATA });
+            ControllerFolder.SelectedIndex = 0;
+            utility_ = new UserFileUtility();
+        }
+
+        private string FormatFolderFile(string folderName, string fileName, string description)
+        {
+            return string.Format("{2} : {0} : {1}", folderName, fileName, description);
+        }
+
+        private void SaveFile(string filePath, string contents)
+        {
+            StreamWriter writer = new StreamWriter(filePath);
+            writer.Write(contents);
+            writer.Close();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog1 = new OpenFileDialog();
+            openFileDialog1.Title = "Select File";
+            openFileDialog1.InitialDirectory = @"C:\";
+            openFileDialog1.Filter = "All Files (*.*)|*.*|Gcode File (*.gcode)|*gcode";
+            openFileDialog1.Multiselect = false;
+            openFileDialog1.FilterIndex = 2;
+            openFileDialog1.ShowDialog();
+            if (openFileDialog1.FileName !="")
+            { textBox1.Text= openFileDialog1.FileName;}
+            else
+            { textBox1.Text= "Secim Yapilmadi!";}
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+        
+            string caption = FormatFolderFile(ControllerFolder.Text, textBox1.Text, button1.Text);
+            try
+            {
+                string error = null;
+                int errorCode = utility_.SendFileToUserDirectory(textBox2.Text, textBox1.Text , ControllerFolder.Text, out error);
+
+                if (errorCode == 0)
+                {
+                    MessageBox.Show("Successful", caption);
+                }
+                else
+                {
+                    MessageBox.Show("Error code [" + errorCode + "]\n" + error, caption);
+                }
+            }
+            catch (Exception exc)
+            {
+                MessageBox.Show(exc.Message, caption);
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
